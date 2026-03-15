0e4e53deca56cc6f9a6a3fba788a90acf06abd85
Diawara Karim <karimdiawara96@gmail.com>
2026-03-15 13:05:03 +0000

    Initial commit

---
+++ app/src/main/java/com/mobile/diafarms/activity/LauncherScreen.java
@@ -0,0 +1,51 @@
+package com.mobile.diafarms.activity;
+
+import android.app.LauncherActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.mobile.diafarms.R;
+
+public class LauncherScreen extends AppCompatActivity {
+
+    private CardView cardLogin, cardQr;
+    private ImageView imgEntete, imgBanner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher_screen);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Liaison des vues
+        cardLogin = findViewById(R.id.cardLogin);
+        cardQr = findViewById(R.id.cardQr);
+        imgEntete = findViewById(R.id.imgEntete);
+        imgBanner = findViewById(R.id.imgBanner);
+
+        // Clic Connexion
+//        cardLogin.setOnClickListener(v -> {
+//            Intent intent = new Intent(LauncherActivity.this, LoginActivity.class);
+//            startActivity(intent);
+//        });
+
+        // Clic QR Code
+//        cardQr.setOnClickListener(v -> {
+//            Intent intent = new Intent(LauncherActivity.this, ScannerActivity.class);
+//            startActivity(intent);
+//        });
+    }
+}
